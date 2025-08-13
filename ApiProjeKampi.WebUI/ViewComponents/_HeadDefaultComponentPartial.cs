2bcb3117dc6eb285c56d2aa29fd964f1c26622fd
Oğuzhan Tarhan <oguzhantarhan7467@gmail.com>
2025-08-13 13:45:48 +0300

    The UI part of the project has been set up, and work on the component partial has begun

---
+++ ApiProjeKampi.WebUI/ViewComponents/_HeadDefaultComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _HeadDefaultComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
