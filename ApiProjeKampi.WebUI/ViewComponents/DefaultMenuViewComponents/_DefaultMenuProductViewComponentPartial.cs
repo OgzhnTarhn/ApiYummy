020cdf985c984d0819e5f8a33bbebb3fa34474c2
Oğuzhan Tarhan <oguzhantarhan7467@gmail.com>
2025-10-18 17:42:28 +0300

    Product apı are consumed

---
+++ ApiProjeKampi.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuProductViewComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using ApiProjeKampi.WebUI.Dtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuProductViewComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultMenuProductViewComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7143/api/Products/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
