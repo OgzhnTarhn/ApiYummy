2dd44c30c0a392562d75293922b34b14052a2833
Oğuzhan Tarhan <oguzhantarhan7467@gmail.com>
2025-10-19 13:09:42 +0300

    _Footer viewComponent partials are created , index.cshtml is updated

---
+++ ApiProjeKampi.WebUI/ViewComponents/_FooterDefaultComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using ApiProjeKampi.WebUI.Dtos.ChefDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _FooterDefaultComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
