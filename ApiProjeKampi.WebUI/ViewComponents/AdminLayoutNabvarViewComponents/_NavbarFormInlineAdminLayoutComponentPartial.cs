38db90551b413ea180a667f13123caa54943a2b1
Oğuzhan Tarhan <oguzhantarhan7467@gmail.com>
2025-10-21 18:50:42 +0300

    Admin Layout Navbar are arranged and making to much more component partials

---
+++ ApiProjeKampi.WebUI/ViewComponents/AdminLayoutNabvarViewComponents/_NavbarFormInlineAdminLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.AdminLayoutNabvarViewComponents
+{
+    public class _NavbarFormInlineAdminLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
