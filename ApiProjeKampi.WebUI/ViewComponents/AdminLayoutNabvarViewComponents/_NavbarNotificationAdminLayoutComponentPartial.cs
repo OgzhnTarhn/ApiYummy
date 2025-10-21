5d7b628b8cf33386eeeb8f633d6518a09a0dd279
Oğuzhan Tarhan <oguzhantarhan7467@gmail.com>
2025-10-21 21:35:11 +0300

    Notification Apı side controllerbase controller is added and uı views are partial component created

---
+++ ApiProjeKampi.WebUI/ViewComponents/AdminLayoutNabvarViewComponents/_NavbarNotificationAdminLayoutComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using ApiProjeKampi.WebUI.Dtos.MessageDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.AdminLayoutNabvarViewComponents
+{
+    public class _NavbarNotificationAdminLayoutComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _NavbarNotificationAdminLayoutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7143/api/Notifications");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMessageByIsReadFalseDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
