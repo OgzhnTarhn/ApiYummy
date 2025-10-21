b9c718cf8d5090c6c088572826478d34d77030c0
Oğuzhan Tarhan <oguzhantarhan7467@gmail.com>
2025-10-21 21:19:28 +0300

    Admin layout messages list in navbar are edited and updated

---
+++ ApiProjeKampi.WebUI/ViewComponents/AdminLayoutNabvarViewComponents/_NavbarMessageListAdminLayoutComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using ApiProjeKampi.WebUI.Dtos.MessageDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.AdminLayoutNabvarViewComponents
+{
+    public class _NavbarMessageListAdminLayoutComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _NavbarMessageListAdminLayoutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7143/api/Messages/MessageByIsReadDtoFalse");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMessageByIsReadFalseDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
