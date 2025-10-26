4ee965d20acedfdc7d186e7bf1a13249819a3ef2
Oğuzhan Tarhan <oguzhantarhan7467@gmail.com>
2025-10-26 20:08:55 +0300

    Update Category is added

--- ApiProjeKampi.WebUI/Dtos/CategoryDtos/GetCategoryByIdDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApiProjeKampi.WebUI.Dtos.CategoryDtos
-{
-    public class GetCategoryByIdDto
-    {
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
-    }
-}
